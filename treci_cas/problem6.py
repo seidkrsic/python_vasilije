214210532b2639ecd639d03e2cd1a7ebe7ffb9e7
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2024-08-28 16:23:08 +0200

    second

--- treci_cas/problem6.py
+++ treci_cas/problem6.py
@@ -1,5 +1,20 @@
 
 
+from datetime import datetime, timedelta
 
+# Get the input date from the user in the format "DD.MM.YYYY"
+input_date = input("Unesite datum u formatu 'DD.MM.YYYY': ")
+day, month, year = map(int, input_date.split(".")) 
+# Get the number of days K
+K = int(input("Unesite broj dana K: "))
 
+# Convert the input string into a datetime object
+date_object = datetime(year, month, day) 
+print(date_object) 
+
+# Add K days to the date
+new_date = date_object + timedelta(days=K)
+
+# Print the new date in the same format "DD.MM.YYYY"
+print(f"Datum nakon dodavanja {K} dana je: {str(new_date)[:10]}") 
 
